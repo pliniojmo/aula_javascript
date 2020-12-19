19ce5697c69ea8041cea743613789c57551f8f18
pliniojmo <plinio.oliveira@gmail.com>
2020-12-18 23:17:03 -0300

    aula01

---
+++ aula-javascript/digital-innovation-one/js/main.js
@@ -0,0 +1,16 @@
+var nome ="Plínio Oliveira";
+var idade = 41;
+var idade2 = 10;
+var n1 =5;
+var n2 =2;
+var frase = "Japão tem o melhor time do mundo.";
+/*alert("meu promeiro js");
+alert("Bom vindo "+nome);*/
+alert(nome+ " tem "+ idade + " anos.");
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+alert(n1*n2);
